2d830add83b8a7d3dccdbc0259c37566dd319ca6
quanganh-snake <tbquanganh@gmail.com>
2024-03-10 23:47:09 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            container: {
+                center: true,
+            },
+            colors: {
+                primary: '#14b8a6',
+                secondary: '#64748b',
+                success: '#10b981',
+                danger: '#ef4444',
+                warning: '#f97316',
+                info: '#0ea5e9',
+                light: '#ffffff',
+                dark: '#030712',
+                muted: '#71717a'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
